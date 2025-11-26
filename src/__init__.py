f381fad2d09a2137678e49c5de09d950ca089475
makaybrose <brosevannessa@gmail.com>
2025-11-26 18:58:19 +0000

    Updated now

--- src/__init__.py
+++ src/__init__.py
@@ -44,12 +44,12 @@ register_exception_handlers(app)
 # ========================
 
 # 2️⃣ IP + Country whitelisting (runs first)
-app.add_middleware(
-    IPWhitelist,
-    allowed_ips=["*"],     # Directly allowed IPs
-    allowed_countries=["GH","US"],      # ISO country code for Ghana
-    geoip_db_path=GEOIP_DB_PATH    # Local GeoIP DB (absolute path)
-)
+#app.add_middleware(
+ #   IPWhitelist,
+  #  allowed_ips=["*"],     # Directly allowed IPs
+   # allowed_countries=["GH","US"],      # ISO country code for Ghana
+    #geoip_db_path=GEOIP_DB_PATH    # Local GeoIP DB (absolute path)
+#)
 
 # 3️⃣ CORS validation (runs after whitelist check)
 app.middleware("http")(cors_validation_middleware)
