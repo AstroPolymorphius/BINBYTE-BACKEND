55e7d3c36b4e041302c87f1513ac57b5a8a7d2f9
Enoch Tetteh Nartey <enochnartey130@gmail.com>
2025-11-28 21:33:48 +0000

    change in the class name

--- src/objects/homepage/models.py
+++ src/objects/homepage/models.py
@@ -3,8 +3,8 @@ from src.models.base import Base
 import uuid 
 from sqlalchemy.dialects.postgresql import UUID
 
-class homepage(Base):
-    __tablename__ = "Homepage"
+class Homepage(Base):
+    __tablenames__ = "Homepages"
     carouselID = Column(UUID(as_uuid=True),primary_key=True,index = True,default=uuid.uuid4)
     title = Column(String, nullable = False)
     subtitle = Column(String, nullable = False)
