d0621a625e3a5d7ab514f5906b0b973a03e573b5
Enoch Tetteh Nartey <enochnartey130@gmail.com>
2025-12-02 10:13:27 +0000

    Done with the product key features model

--- src/objects/projectkeyfeature/models.py
+++ src/objects/projectkeyfeature/models.py
@@ -1,10 +1,18 @@
+from typing import Optional
 from sqlalchemy import Column,String,Integer,Float,DateTime,ForeignKey
-from src.models.base import Base
-import uuid 
-from sqlalchemy.dialects.postgresql import UUID
+from src.models.base import UUIDModel, TimestampedModel
+from uuid import UUID
+from sqlalchemy.dialects.postgresql import UUID as pgUUID
+from sqlalchemy.orm import Mapped, mapped_column
 
-class ProjectKeyFeatures(Base):
+
+class ProjectKeyFeatures(UUIDModel, TimestampedModel):
     __tablenames__ = "Product key features"
-    keyfeaturesID = Column(UUID(as_uuid=True),primary_key=True,index = True,default=uuid.uuid4)
-    keyfeature= Column(String, nullable = False)
-    projectID =  Column(UUID(as_uuid=True),primary_key=True,index = True,default=uuid.uuid4) #(FK)
+    keyfeaturesID: Mapped[UUID] =  mapped_column(pgUUID(as_uuid=True),primary_key=True,index = True)
+    keyfeature: Mapped[str] = mapped_column(String, nullable=False)
+    projectID: Mapped[UUID] = mapped_column(pgUUID(as_uuid=True),primary_key=True,index = True)  # FK
+
+
+
+
+   
