61cf8e88fc7250233540a83b6983fd1bb6f722e7
Acheampong Panin <panindarkwah@gmail.com>
2025-12-04 21:58:33 +0000

    Did my best with this #13

--- src/objects/project_assigment/project.py
+++ src/objects/project_assigment/project.py
@@ -1,12 +1,8 @@
-import uuid
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, DateTime
-from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import String
 from src.models.base import UUIDModel, TimestampedModel
 class ProjectMember(UUIDModel, TimestampedModel):
-    member_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     email: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
-    project_id: Mapped[str] = mapped_column(String, nullable=False)
     description: Mapped[str] = mapped_column(String, nullable=False)
     role: Mapped[str] = mapped_column(String, nullable=False)
     tech_used: Mapped[str] = mapped_column(String, nullable=False)
