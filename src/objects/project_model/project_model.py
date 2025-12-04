033fc68f7b714d05a2ea61b8fd09e86a0262e78f
James <jamese.spaceworld@gmail.com>
2025-12-04 18:01:56 +0000

    Created a new project_model in objects and defined project database model. The model includes fields such as projectname, category, status, completed, duration, about, githubURL and websiteURL.

--- src/objects/project_model/project_model.py
+++ src/objects/project_model/project_model.py
@@ -1,19 +1,22 @@
-#Importing the base model and uuid for the primary key
-from sqlalchemy import Column, String
-from database import Base
-import uuid
-from sqlalchemy.dialects.postgresql import UUID
+from typing import Optional
+from uuid import UUID
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID as pgUUID
+from sqlalchemy import Column, String, Date
+from datetime import date
+
+from src.models.base import UUIDModel, TimestampedModel
 
 #Defines the project model in the database
-class Project(Base):
+class Project(UUIDModel, TimestampedModel):
     __tablename__ = "projects"
 
-    projectID = Column(UUID(as_uuid = True), primary_key = True, index = True, default = uuid.uuid4, nullable= False)
-    projectname = Column(String, index = True, nullable= False)
-    category = Column(String, index = True, nullable= False)
-    status = Column(String, index = True, nullable= False)
-    completed = Column(String, nullable= True)
-    duration = Column(String, nullable= True)
-    about = Column(String, nullable= True)
-    githubURL = Column(String, index = True, unique = True, nullable= True)
-    websiteURL = Column(String, index = True, nullable= True)
+    projectname = Mapped[str] = mapped_column(String, nullable= False)
+    category = Mapped[str] = mapped_column(String, nullable= False)
+    status = Mapped[str] = mapped_column(String, nullable= False)
+    completed = Mapped[date] = mapped_column(Date, nullable= True)
+    duration = Mapped[str] = mapped_column(String, nullable= True)
+    about = Mapped[str] = mapped_column(String, nullable= True)
+    githubURL = Mapped[str] = mapped_column(String, unique = True, nullable= True)
+    websiteURL = Mapped[str] = mapped_column(String, nullable= True)
