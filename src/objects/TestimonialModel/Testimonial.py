f8905e273ef6b69964117e37adc7cfe0361f91ac
Mimi-cell-ash <gale.wepiya@gmail.com>
2025-12-04 21:44:14 +0000

    Testimonial model completed closes #1

--- src/objects/TestimonialModel/Testimonial.py
+++ src/objects/TestimonialModel/Testimonial.py
@@ -8,12 +8,14 @@ from sqlalchemy import String, Integer, Integer, DateTime
 
 from src.models.base import UUIDModel, TimestampedModel
 
+
 ##Testimonial Model
+
 class Testimonial(UUIDModel, TimestampedModel):
     full_name: Mapped[str] = mapped_column(String, nullable=False)
     feedback: Mapped[str] = mapped_column(String, nullable=False)  
     rating: Mapped[int] = mapped_column(Integer, nullable=False)   
     fields_of_training: Mapped[str] = mapped_column(String, nullable=False)    
-    date_of_testimonial:  Mapped[datetime] =mapped_column(DateTime,nullable=True, default=datetime.utcnow)
+    date_of_testimonial:  Mapped[datetime] =mapped_column(DateTime,nullable=True, default=datetime.utcnow)     
 
 
