61c39d1c6fba118c3f248cccf11eb11d896abc2b
makaybrose <brosevannessa@gmail.com>
2025-12-05 22:23:01 +0000

    adedd closes #3

--- src/object/member_model.py
+++ src/object/member_model.py
@@ -1,4 +1,4 @@
-from typing import Optional
+
 from sqlalchemy import Column, Integer, String,Float
 from src.models.base import UUIDModel,TimestampedModel
 from uuid import UUID # for generating unique IDs(this is from python standard library)
@@ -6,14 +6,11 @@ from sqlalchemy.dialects.postgresql import UUID as pgUUID
 from sqlalchemy.orm import  Mapped, mapped_column
 
 class Member(UUIDModel,TimestampedModel):
-    __tablename__ = 'Members'
-
-    id = Mapped[UUID] = mapped_column(pgUUID(as_uuid=True), primary_key=True)
     firstname =Mapped[str] = mapped_column(String, nullable=False)
     lastname =Mapped[str] = mapped_column(String, nullable=False)
     role =Mapped[str] = mapped_column(String, nullable=False)
     connections =Mapped[str] = mapped_column(String, nullable=False)
     experience =Mapped[int] = mapped_column(Integer, nullable=False)
     bio =Mapped[Optional[str]] = mapped_column(String, nullable=True)
-    date_joined =Mapped[str] = mapped_column(String, nullable=False)
+    
     
