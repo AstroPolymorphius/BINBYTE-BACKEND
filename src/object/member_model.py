112f8037cbf2633ef70f72e942433c51316800c8
makaybrose <brosevannessa@gmail.com>
2025-12-05 22:34:48 +0000

    assigned closes #3

--- src/object/member_model.py
+++ src/object/member_model.py
@@ -1,15 +1,17 @@
 
-from sqlalchemy import Column, Integer, String,Float
+from sqlalchemy import Integer, String
 from src.models.base import UUIDModel,TimestampedModel
 from uuid import UUID # for generating unique IDs(this is from python standard library)
 from sqlalchemy.orm import  Mapped, mapped_column
 
+#meber model
 class Member(UUIDModel,TimestampedModel):
+    
     firstname =Mapped[str] = mapped_column(String, nullable=False)
     lastname =Mapped[str] = mapped_column(String, nullable=False)
     role =Mapped[str] = mapped_column(String, nullable=False)
     connections =Mapped[str] = mapped_column(String, nullable=False)
     experience =Mapped[int] = mapped_column(Integer, nullable=False)
-    bio =Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    bio =Mapped[str] = mapped_column(String, nullable=True)
     
     
